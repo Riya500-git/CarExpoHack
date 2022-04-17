a6ba1fcdf878ed043aad8467ee741f7dbdb2e48f
TRucker20 <83630849+TRucker20@users.noreply.github.com>
2022-04-17 06:44:00 -0700

    Update text.py

--- text.py
+++ text.py
@@ -9,11 +9,11 @@ def textme():
     account_sid = 'AC1aa61ef4d890feb1931e8f26c3f7e7b6'
     auth_token = 'eb1cfa1e3a5a693c932b75579a4c219c'
     client = Client(account_sid, auth_token)
-
+# sends message to user
     message = client.messages \
         .create(
             body='Your car is getting towed!',
             from_='+19032731446',
-            to='+16194058727'
+            to='User Phone #'
         )
     print(message.sid)
